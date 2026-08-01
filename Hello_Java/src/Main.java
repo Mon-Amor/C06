5c7154b98581d20da6300a4feb4c25ce408e8b8e
Monique Amorim <143218972+Mon-Amor@users.noreply.github.com>
2026-07-31 22:24:44 -0300

    Aula 01 - Introdução
    31/07/2026

---
+++ Hello_Java/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("OLÁ JAVA!!!!!");
+        int x = 10;
+        int y = 10;
+        System.out.println("O resultado é:"+ (x+y));
+    }
+}
