60fda087df02a85ea18122577e608d69f2cf9823
Monique Amorim <143218972+Mon-Amor@users.noreply.github.com>
2026-08-20 23:32:21 -0300

    Aula 03 - Exercício 01

---
+++ Aula 03 - Exercicio 01/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("OLÁ JAVA!!!!!");
+        int lanche1 = 10;
+        int lanche2 = 4;
+        int  lanche3 = 2;
+        int media = (lanche1+lanche2+lanche3/3);
+        System.out.println("O número de lanches consumidos é:"+ (lanche1+lanche2+lanche3));
+        System.out.println("A média é:"+ (media));
+    }
+}
